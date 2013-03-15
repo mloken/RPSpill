8dbd61b4540f8d67b824a14da395f7cdead2de18
Bent Arnesen <bentcore@gmail.com>
2013-03-15 20:23:06 +0100

    Nå da

---
+++ RPSpill_0.1/src/Viewer/CreateCharacter.java
@@ -0,0 +1,19 @@
+package Viewer;
+
+import java.util.Scanner;
+import Model.Karakter;
+
+public class CreateCharacter {
+
+	Karakter ch = new Karakter();
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public CreateCharacter() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
