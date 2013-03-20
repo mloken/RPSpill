bcb49352071cd3e30ef3b1628a0d697ffca12592
Bent Arnesen <bentcore@gmail.com>
2013-03-20 20:14:24 +0100

    Klasseoppdatering. Databaseoppdatering (tables)

--- RPSpill_0.1/src/Model/Item.java
+++ RPSpill_0.1/src/Model/Item.java
@@ -6,34 +6,27 @@ public class Item {
 
 	public String name = "";
 	public String rClass = "";
-	public int ID;
-	public int str, dex, intel, AtkDmg, SpellDmg, hp, mp;
-	public int rStr, rDex, rInt, rLevel, quality;
+	public String type = "";
+	public int str, dex, intel, AtkDmg;
+	public int level, quality;
 	
 	public Item() throws Exception {
 		
-		JDBC.addItems(this.createWeapon(0, "Sword #1", "Common", 0, 10, 10, 10, 10, 10, 0, 0, 0, 1));
-		JDBC.addItems(this.createWeapon(1, "BentSword", "Master", 100, 1000, 1000, 1000, 1000, 1000, 0, 0, 0, 5));
-		JDBC.addItems(this.createWeapon(2, "Bow", "Ranger", 10, 5, 10, 0, 15, 0, 0, 0, 0, 1));
-	}
-	
-	public void createPotion(int pID, String name, int hp, int mp) {
-		
+		JDBC.addItems(this.createWeapon("Sword #1", "Common", "Sword", 1, 10, 10, 10, 10, 1));
+		JDBC.addItems(this.createWeapon("BentSword", "Master", "Sword", 100, 1000, 1000, 1000, 1000, 5));
+		JDBC.addItems(this.createWeapon("Bow", "Ranger", "Bow", 10, 5, 10, 0, 15, 1));
 	}
 
 	// This should probably be done in another way..? 
-	public Item createWeapon(int wID, String name, String rClass, int rLevel, int str, int dex, int intel,
-			int AtkDmg, int SpellDmg, int rStr, int rDex, int rInt, int quality) {
+	public Item createWeapon(String name, String rClass, String type, int level, int str, int dex, int intel,
+			int AtkDmg, int quality) {
 		
-		this.ID = wID; this.name = name;
+		this.name = name;
+		this.rClass = rClass;
+		this.type = type;
+		this.level = level;
 		this.str = str;	this.dex = dex;	this.intel = intel;
 		this.AtkDmg = AtkDmg;
-		this.SpellDmg = SpellDmg;
-		this.rClass = rClass;
-		this.rLevel = rLevel;
-		this.rStr = rStr;
-		this.rDex = rDex;
-		this.rInt = rInt;
 		this.quality = quality;
 		
 		return this;
@@ -41,14 +34,17 @@ public class Item {
 	
 	public String printWeapon() {
 		
-		String Requirement = "\n  -Requirements-\n  Class: " + this.rClass + "\n  Lvl:" + this.rLevel +
-				"\n  Str:" + this.rStr + "\n  Dex:" + this.rDex + "\n  Int:" + this.rInt;
+		String Requirement = "\n  -Requirements-\n  Class: " + this.rClass + "\n  Lvl:" + this.level;
+				
 		String s = "" + this.name +  Requirement +
 				"\n    Attack damage: " + this.AtkDmg +
-				"\n    Magic damage: " + this.SpellDmg +
 				"\n    Str: " + this.str
 				+ "\n    Dex: " + this.dex + "\n    Int: " + this.intel  + 
 				"\n       Quality: " + this.quality;
 		return s;
+	}
+	
+	public void createPotion(int pID, String name, int hp, int mp) {
+		
 	}
 }
--- RPSpill_0.1/src/marcus/WeaponTest.java
+++ RPSpill_0.1/src/marcus/WeaponTest.java
@@ -7,7 +7,7 @@ public class WeaponTest {
 	public static void main(String[] args) {
 
 		ArrayList<Sword> swordList = new ArrayList<Sword>();
-		int level = 90;
+		int level = 1;
 		
 		for(int i = 0; i < 20; i++){
 			swordList.add(new Sword(level));
