830e3ff4c582e8483a8ec0469d0f2cc1e903336d
Marcus Løken <lm.loken@gmail.com>
2013-03-21 14:25:50 +0100

    do while i Run.class

--- RPSpill_0.1/src/Controller/Run.java
+++ RPSpill_0.1/src/Controller/Run.java
@@ -9,15 +9,21 @@ public class Run {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Username: ");
-		owner = sc.nextLine();
-		System.out.print("Password: ");
-		String p = sc.nextLine();
-		boolean a = (JDBC.logIn(owner, p));
-		if (!a) {
-			System.out.println("Wrong username or password.");
-			return;
-		}
+		
+		do{
+			System.out.print("Username: ");
+			owner = sc.nextLine();
+			System.out.print("Password: ");
+			String p = sc.nextLine();
+			boolean a = (JDBC.logIn(owner, p));
+			if (!a) {
+				System.out.println("Wrong username or password.");
+				continue;
+			}
+			else
+				break;
+		}while(true);
+		
 		System.out.println("Logged in as: " + owner.toLowerCase());
 		System.out.println("Press 1 to choose existing character.");
 		System.out.println("Press 2 to create a character.");
